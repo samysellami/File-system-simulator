f033644644da97dae1ad6b7aae8dcd307a38ada5
Sami SELLAMI <s.sellami@innopolis.university>
2020-07-26 15:16:36 +0300

    Add files via upload
    adding files

---
+++ cmake-build-debug/CMakeFiles/File_system_simulator.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/Innopolis Courses/Low level and embedded programming/Assignements/Assignment1/File system simulator/main.c" "D:/Innopolis Courses/Low level and embedded programming/Assignements/Assignment1/File system simulator/cmake-build-debug/CMakeFiles/File_system_simulator.dir/main.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../.idea"
+  "../bin"
+  "."
+  "../obj"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/File_system_simulator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/File_system_simulator.dir/main.obj"
+  "File_system_simulator.pdb"
+  "File_system_simulator.exe"
+  "File_system_simulator.exe.manifest"
+  "libFile_system_simulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/File_system_simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
